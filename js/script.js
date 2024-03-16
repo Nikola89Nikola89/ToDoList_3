55b41ddf27357d6fc7556794ba7bcf01afdf63a3
Nikola89Nikola89 <105931981+Nikola89Nikola89@users.noreply.github.com>
2024-03-16 13:05:12 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const button = document.querySelector('.input__container button');
+const input = document.querySelector('.input__container input');
+const list = document.querySelector('.todo-list');
+
+button.addEventListener('click', () =>{
+    const li = document.createElement('li');
+    li.className = 'todo-list-item';
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Delete';
+    li.innerText = input.value;
+    list.appendChild(li);
+    li.appendChild(deleteBtn);
+    input.value = "";
+
+    deleteBtn.addEventListener('click', () =>{
+        list.removeChild(li);
+    });
+});
